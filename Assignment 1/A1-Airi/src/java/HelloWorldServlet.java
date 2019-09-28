2ff2b9d35bb87f5bfdb338a2c18f0126ad70c074
airi-14x <airi.14x@gmail.com>
2019-09-28 14:31:04 -0400

    Add Functional Task1 - text version

--- Assignment 1/A1-Airi/src/java/HelloWorldServlet.java
+++ Assignment 1/A1-Airi/src/java/HelloWorldServlet.java
@@ -6,6 +6,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,8 +33,8 @@ public class HelloWorldServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             //localhost:8080/A1-Airi/HelloWorldServlet?format=text&param1=val1&param2=val2
+            //localhost:8080/A1-Airi/HelloWorldServlet?format=text&param1=１３２&param2=val2
             out.println("<!DOCTYPE xhtml>");
             out.println("<html>");
             out.println("<head>");
@@ -41,41 +42,41 @@ public class HelloWorldServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet HelloWorldServlet at " + request.getContextPath() + "</h1>");
-            out.println("<h4>Request Method: " + request.getMethod() + "</h4>");
-            out.println("<h4>Request Headers: </h4>");
-            out.println("<h4>Host: " + request.getHeader("host") + "</h4>");
-            out.println("<h4>Connection: " + request.getHeader("connection") + "</h4>");
-            out.println("<h4>User-Agent: " + request.getHeader("user-agent") + "</h4>");
-            out.println("<h4>Accept: " + request.getHeader("accept") + "</h4>");
-            out.println("<h4>Query String: </h4>");
 
             if (request.getParameter("format") == null) {
                 current_format = "html";
-                out.println("<h4>format: " + current_format + "</h4>");
-                out.println("");
-                out.println("Response:");
                 response.setStatus(200);
-                out.println(request.getProtocol());
-                out.println(response.getStatus() + " OK");
 
             } else if (!request.getParameter("format").equals("text") && !request.getParameter("format").equals("html")
                     && !request.getParameter("format").equals("xml")) {
-                out.println("");
                 response.setStatus(400); //Bad Request
                 out.println("<h4> Response: " + request.getProtocol() + " " + response.getStatus()
                         + " Bad Request - Syntax error in Request</h4>");
-                //out.println("<h4>format: " + "ERROR PAGE NEEDED TO BE DISPLAYED!" + "</h4>");
             } else {
                 current_format = request.getParameter("format");
-                out.println("<h4>format: " + current_format + "</h4>");
-                out.println("");
-                out.println("Response:");
                 response.setStatus(200);
-                out.println(request.getProtocol());
-                out.println(response.getStatus() + " OK");
             }
-            //out.println(request.getQueryString());
-            //Enumeration<String> request2 = request.getParameterNames();
+
+            if (response.getStatus() == 200 && current_format.equals("text")) {
+
+                out.println("<h4>Request Method: " + request.getMethod() + "</h4>");
+                out.println("<h4>Request Headers: </h4>");
+                out.println("<h4> - Host: " + request.getHeader("host") + "</h4>");
+                out.println("<h4> - Connection: " + request.getHeader("connection") + "</h4>");
+                out.println("<h4> - User-Agent: " + request.getHeader("user-agent") + "</h4>");
+                out.println("<h4> - Accept: " + request.getHeader("accept") + "</h4>");
+                out.println("<h4> - Query String: </h4>");
+
+                String[] query_strings = URLDecoder.decode(request.getQueryString(), "UTF-8").split("&");
+
+                int index = 0;
+                for (String temp_str : query_strings) {
+                    temp_str = temp_str.replace("=", ": ");
+                    query_strings[index] = temp_str;
+                    index++;
+                    out.println("<h4>&nbsp &nbsp &nbsp &nbsp" + temp_str + "</h4>");
+                }
+            }
 
             current_format = "";
             out.println("</body>");

02177f33932b6ad0814c871cf43059282ffc4d16
airi-14x <airi.14x@gmail.com>
2019-10-05 14:11:15 -0400

    Modifications to make CSS work

--- Assignment 1/A1-Airi/src/java/SOEN387/A1HttpServer.java
+++ Assignment 1/A1-Airi/src/java/SOEN387/A1HttpServer.java
@@ -4,7 +4,6 @@ import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,37 +28,20 @@ public class A1HttpServer {
         HttpContext index_context = server.createContext("/");
         HttpContext another_index_context = server.createContext("/index.html");
         HttpContext index2_context = server.createContext("/index2.html");
-        HttpContext css_context = server.createContext("/css/css-file1.css");
-        HttpContext css2_context = server.createContext("/css/css-file2.css");
+        HttpContext css_context = server.createContext("/css/css-file1_1.css");
+        HttpContext css2_context = server.createContext("/css/css-file2_1.css");
 
         index_context.setHandler(A1HttpServer::handleIndexRequest);
         another_index_context.setHandler(A1HttpServer::handleIndexRequest);
         index2_context.setHandler(A1HttpServer::handleIndex2Request);
-        //css_context.setHandler(A1HttpServer::handleCSSRequest);
-        //css_context.setHandler(A1HttpServer::handleCSS2Request);
+        css_context.setHandler(A1HttpServer::handleCSSRequest);
+        css2_context.setHandler(A1HttpServer::handleCSS2Request);
 
         server.start();
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter something to stop server: ");
         String input_value = reader.next();
         server.stop(0);
-    }
-
-    private static void handleRequest(HttpExchange exchange) throws IOException {
-        String response = "Hi there!";
-
-        Scanner scanner = new Scanner(new File("index.html"));
-        String temp_input = "";
-        while (scanner.hasNext()) {
-            temp_input += scanner.next();
-        }
-        System.out.println(temp_input);
-        scanner.close();
-        //exchange.sendResponseHeaders(200, temp_input.getBytes().length);
-        //exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
-        OutputStream os = exchange.getResponseBody();
-        os.write(temp_input.getBytes());
-        os.close();
     }
 
     private static void handleIndexRequest(HttpExchange exchange) throws IOException {
@@ -70,8 +52,44 @@ public class A1HttpServer {
         // Find index.html file --> /web/index.html //
         try (FileReader index_file = new FileReader(current_path + "/web/index.html");
                 BufferedReader bufferedReader = new BufferedReader(index_file);) {
-            //FileReader index_file = new FileReader(current_path + "/web/index.html");
-            //BufferedReader bufferedReader = new BufferedReader(index_file);
+            // Read File //
+            String current_line = "";
+
+            while ((current_line = bufferedReader.readLine()) != null) {
+                response += current_line;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found error!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error reading the file!");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Get Context URI //
+        String request_URI = exchange.getRequestURI().toString();
+
+        exchange.getResponseHeaders().set("Content-Type", "text/html");
+
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        } catch (Exception e) {
+            System.out.println("Error with OutputStream");
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleIndex2Request(HttpExchange exchange) throws IOException {
+
+        // Get current path //
+        String current_path = System.getProperty("user.dir");
+        String response = "";
+        // Find index2.html file --> /web/index2.html //
+        try (FileReader index_file = new FileReader(current_path + "/web/index2.html");
+                BufferedReader bufferedReader = new BufferedReader(index_file);) {
 
             // Read File //
             String current_line = "";
@@ -91,28 +109,94 @@ public class A1HttpServer {
         }
         // Get Context URI //
         String request_URI = exchange.getRequestURI().toString();
+
         exchange.getResponseHeaders().set("Content-Type", "text/html");
+
         exchange.sendResponseHeaders(200, response.getBytes().length);
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes());
+        } catch (Exception e) {
+            System.out.println("Error with OutputStream");
+            e.printStackTrace();
         }
-    }
-
-    private static void handleIndex2Request(HttpExchange exchange) throws IOException {
-
-        // Get current path //
-        String current_path = System.getProperty("user.dir");
-
-        // Find index.html file --> /web/index2.html //
-        //File index2_file = new File(current_path + "/web/index2.html");
     }
 
     private static void handleCSSRequest(HttpExchange exchange) throws IOException {
 
+        // Get current path //
+        String current_path = System.getProperty("user.dir");
+        String response = "";
+        // Find css-file1_1 file --> /web/css/css-file1_1.css //
+        try (FileReader index_file = new FileReader(current_path + "/web/css/css-file1_1.css");
+                BufferedReader bufferedReader = new BufferedReader(index_file);) {
+
+            // Read File //
+            String current_line = "";
+
+            while ((current_line = bufferedReader.readLine()) != null) {
+                response += current_line;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found error!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error reading the file!");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Get Context URI //
+        String request_URI = exchange.getRequestURI().toString();
+
+        exchange.getResponseHeaders().set("Content-Type", "text/css");
+
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        } catch (Exception e) {
+            System.out.println("Error with OutputStream");
+            e.printStackTrace();
+        }
     }
 
     private static void handleCSS2Request(HttpExchange exchange) throws IOException {
 
+        // Get current path //
+        String current_path = System.getProperty("user.dir");
+        String response = "";
+        // Find css-file1_1 file --> /web/css/css-file1_1.css //
+        try (FileReader index_file = new FileReader(current_path + "/web/css/css-file2_1.css");
+                BufferedReader bufferedReader = new BufferedReader(index_file);) {
+
+            // Read File //
+            String current_line = "";
+
+            while ((current_line = bufferedReader.readLine()) != null) {
+                response += current_line;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found error!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error reading the file!");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Get Context URI //
+        String request_URI = exchange.getRequestURI().toString();
+
+        exchange.getResponseHeaders().set("Content-Type", "text/css");
+
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        } catch (Exception e) {
+            System.out.println("Error with OutputStream");
+            e.printStackTrace();
+        }
     }
 
 }

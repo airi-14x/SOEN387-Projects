144a09bddb8436f10f12f33e733bb3794ebc3adb
airi-14x <airi.14x@gmail.com>
2019-10-26 14:50:26 -0400

    Init A2 business layer classes

--- Assignment 2/A2-Airi/database-init.sql
+++ Assignment 2/A2-Airi/database-init.sql
@@ -1,10 +1,20 @@
 CREATE TABLE `book`(
-	`id` int(11) NOT NULL,
-    `title` varchar(64) DEFAULT NULL,
-    `description` varchar(256) DEFAULT NULL,
-    `isbn` varchar(64) DEFAULT NULL,
-    `last_name` varchar(64) DEFAULT NULL,
-    `first_name` varchar(64) DEFAULT NULL,
-    `publisher_company` varchar(64) DEFAULT NULL,
-    `address` varchar(64) DEFAULT NULL
+	`id` VARCHAR(11) NOT NULL,
+    `title` VARCHAR(64) DEFAULT NULL,
+    `description` VARCHAR(256) DEFAULT NULL,
+    `isbn` VARCHAR(64) DEFAULT NULL,
+    `last_name` VARCHAR(64) DEFAULT NULL,
+    `first_name` VARCHAR(64) DEFAULT NULL,
+    `publisher_company` VARCHAR(64) DEFAULT NULL,
+    `address` VARCHAR(64) DEFAULT NULL,
+    `image_mime` VARCHAR(256) DEFAULT NULL,
+    `image_data` BLOB
+);
+
+CREATE PROCEDURE updateidCounter (
+id VARCHAR(11)
 )
+BEGIN
+	SELECT id, title FROM book
+    WHERE this.id = id;
+END;

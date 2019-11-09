df592b22fe3bc2ec6c021ecab8233ccc72a60f97
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-11-09 12:32:09 -0500

    conflict fix

--- Assignment 2/A2-Repository/src/repository/core/Session.java
+++ Assignment 2/A2-Repository/src/repository/core/Session.java
@@ -25,6 +25,12 @@ public class Session {
 
     private static JSONObject currentUser = null;
     
+    public Session() throws IOException {
+        createJsonObject();
+        
+    }
+    
+ 
 
     public String hashPassword(String password) {
         String hashedPassword = null;

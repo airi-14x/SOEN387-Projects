80f8502874a7ee71bc04a7fab063f5719bb18040
airi-14x <airi.14x@gmail.com>
2019-11-14 18:49:03 -0500

    Singleton pattern

---
+++ Labs/Lab7/Lab8-DesignPatterns/src/lab8/designpatterns/Singleton.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8.designpatterns;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Airi
+ */
+//
+enum EnumSingleton {
+    WEEKDAY("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"),
+    WEEKEND("Saturday", "Sunday");
+
+    private String[] days;
+
+    EnumSingleton(String... days) {
+        System.out.println("Initializing enum with " + Arrays.toString(days));
+        this.days = days;
+    }
+
+    public String[] getDays() {
+        return this.days;
+    }
+
+    public String toString() {
+        return "EnumSingleton{"
+                + "days=" + Arrays.toString(days) + '}';
+    }
+}
+
+/*
+    enumEnumSingleton {
+WEEKDAY("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"),
+WEEKEND("Saturday", "Sunday");
+privateString[] days;
+EnumSingleton(String ...days) {
+System.out.println("Initializing enum with "+ Arrays.toString(days));
+this.days= days;
+}
+publicString[] getDays() {
+returnthis.days;
+}
+@Override
+publicString toString() {
+return"EnumSingleton{"+
+"days="+ Arrays.toString(days) +
+'}';
+}
+}
+
+
+ */
+//}
